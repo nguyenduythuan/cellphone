34466eac5b2aea5b1445996725e318a5d3d19d7a
nguyenduythuan <thuanduy73h1@gmail.com>
2019-11-12 00:57:59 +0700

    done header

--- top/js/script.js
+++ top/js/script.js
@@ -4,11 +4,22 @@
         let mParent = $(this).parent();
         mParent.toggleClass('Menu-branch-search-loaction--active');
       });
+
       $('.js-locationOption').click(function(){
         let mLocation = $(this).find('span').text();
         let mBtn = $('.js-locationBtn');
         mBtn.text(mLocation);
         mBtn.parent().removeClass('Menu-branch-search-loaction--active');
+      });
+
+      $('.js-navHamberger').click(function () {
+        let mNav = $('.js-nav');
+        mNav.toggleClass('Nav--active');
+        if(mNav.hasClass('Nav--active')) {
+          $('body').addClass('Noscroll');
+        }else {
+          $('body').removeClass('Noscroll');
+        }
       });
   });
 })(jQuery);
