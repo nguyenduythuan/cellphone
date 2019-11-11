d50567c0aeb25c51faea9f47d643da2a948ab3d3
nguyenduythuan <thuanduy73h1@gmail.com>
2019-11-11 16:34:51 +0700

    update menu pc and set up nav sp

--- top/js/script.js
+++ top/js/script.js
@@ -1,5 +1,14 @@
 (function($) {
   $(function() {
-      $('.js-menuListItem').hover();
+      $('.js-locationBtn').click(function () {
+        let mParent = $(this).parent();
+        mParent.toggleClass('Menu-branch-search-loaction--active');
+      });
+      $('.js-locationOption').click(function(){
+        let mLocation = $(this).find('span').text();
+        let mBtn = $('.js-locationBtn');
+        mBtn.text(mLocation);
+        mBtn.parent().removeClass('Menu-branch-search-loaction--active');
+      });
   });
 })(jQuery);
