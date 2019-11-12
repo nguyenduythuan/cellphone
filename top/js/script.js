68f21c4d0cc29448960a174dee92988817291efc
nguyenduythuan <thuanduy73h1@gmail.com>
2019-11-12 16:35:23 +0700

    lam banner

--- top/js/script.js
+++ top/js/script.js
@@ -1,25 +1,33 @@
 (function($) {
   $(function() {
-      $('.js-locationBtn').click(function () {
-        let mParent = $(this).parent();
-        mParent.toggleClass('Menu-branch-search-loaction--active');
-      });
+    $(".js-locationBtn").click(function() {
+      let mParent = $(this).parent(); //lay cha cua the click
+      mParent.toggleClass("Menu-branch-search-loaction--active"); //them class neu chua co xoa classneu co roi
+    });
 
-      $('.js-locationOption').click(function(){
-        let mLocation = $(this).find('span').text();
-        let mBtn = $('.js-locationBtn');
-        mBtn.text(mLocation);
-        mBtn.parent().removeClass('Menu-branch-search-loaction--active');
-      });
+    $(".js-locationOption").click(function() {
+      let mLocation = $(this)
+        .find("span") //tim tat ca cac the span
+        .text();
+      let mBtn = $(".js-locationBtn");
+      mBtn.text(mLocation);
+      mBtn.parent().removeClass("Menu-branch-search-loaction--active");
+    });
 
-      $('.js-navHamberger').click(function () {
-        let mNav = $('.js-nav');
-        mNav.toggleClass('Nav--active');
-        if(mNav.hasClass('Nav--active')) {
-          $('body').addClass('Noscroll');
-        }else {
-          $('body').removeClass('Noscroll');
-        }
-      });
+    $(".js-navHamberger").click(function() {
+      let mNav = $(".js-nav");
+      mNav.toggleClass("Nav--active");
+      if (mNav.hasClass("Nav--active")) {
+        $("body").addClass("Noscroll");
+      } else {
+        $("body").removeClass("Noscroll");
+      }
+    });
+
+    $(window).on("load resize", function() {
+      let mWidth = $(".js-carousel").width(); //lay chieu rong
+      let mHeight = mWidth / 2.665;
+      $(".js-carouselCtx").height(mHeight); // set chieu cao
+    });
   });
 })(jQuery);
